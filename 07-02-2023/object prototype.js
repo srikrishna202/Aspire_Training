7087ee37c1fd13c2f7f98d29406605c112cfbe23
srikrishna202 <113791572+srikrishna202@users.noreply.github.com>
2023-02-08 19:09:14 +0530

    Add files via upload

---
+++ 07-02-2023/object prototype.js
@@ -0,0 +1,25 @@
+//properties chain:
+
+const myDate = new Date();
+let object = myDate;
+
+do {
+  object = Object.getPrototypeOf(object);
+  console.log(object);
+} while (object);
+/*
+// Date.prototype
+// Object { }
+// null
+*/
+
+//shodowing properties
+const myDate = new Date(1995, 11, 17);
+
+console.log(myDate.getYear()); // 95
+
+myDate.getYear = function () {
+  console.log("something else!");
+};
+
+myDate.getYear();
