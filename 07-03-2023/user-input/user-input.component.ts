ceef0e5986b8993bec9c431f84220d139d6873f8
srikrishna202 <113791572+srikrishna202@users.noreply.github.com>
2023-03-07 18:23:25 +0530

    Add files via upload

---
+++ 07-03-2023/user-input/user-input.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-input',
+  // templateUrl: './user-input.component.html',
+  templateUrl: './../app.component.html',
+  styleUrls: ['./user-input.component.css']
+})
+export class UserInputComponent {
+
+}
+
+/*
+ngOnchanges() -> this method is invoke whenever an input properties is changed.
+ngOnint() -> whenever once component is initialized then it invoked {constructor next ngOnInit } 
+ngDoCheck() -> it call everytime whenever changes in component
+ngAfterContentInit -> 
+ng
+*/
